bd28e48e01867fc22172fe8da78853508aac4907
thewarsawpakt <smallperson07@gmail.com>
2021-02-08 15:56:40 -0600

    first commit

---
+++ twirl.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#
+#  Twirl package manager
+#
+#  Copyright 2021 VideoToaster <glitchyneopet@gmail.com>
+#  Copyright 2021 Thewarsawpakt <thewarsawpakt@gmail.com>
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the LGPL.
+
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    
+)
